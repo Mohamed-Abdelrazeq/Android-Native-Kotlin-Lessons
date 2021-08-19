0adf36977e1f9367515bde5bf75ca6ebf287d560
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2021-08-19 14:47:40 +0200

    Lesson#3 Multiple Activities

--- app/src/main/java/com/example/unit1/MainActivity.kt
+++ app/src/main/java/com/example/unit1/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.unit1
 
-import android.annotation.SuppressLint
-import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import androidx.databinding.DataBindingUtil
 import com.example.unit1.databinding.ActivityMainBinding
 
@@ -16,25 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        binding.btnAdd.setOnClickListener {
-            addName(it)
-            println("Pressed")
+        binding.btnForward.setOnClickListener {
+            Intent(this,SecondActivity::class.java).also {
+                startActivity(it)
+            }
         }
 
     }
 
-    private fun addName(view: View){
-        binding.apply {
-            tvName.text = "Hello ${etName.text}"
-            etName.visibility = View.GONE
-            btnAdd.visibility = View.GONE
-        }
-
-        // Hide the keyboard.
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, 0)
-    }
 }
---
+++ app/src/main/java/com/example/unit1/SecondActivity.kt
@@ -0,0 +1,18 @@
+package com.example.unit1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        btnBackward.setOnClickListener {
+             finish()
+        }
+
+    }
+}
