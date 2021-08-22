0b690418458708a82d846da2eaa2be503d8f7957
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2021-08-22 12:56:44 +0200

    Lesson#4 Permissions Image Picker

--- app/src/main/java/com/example/unit1/MainActivity.kt
+++ app/src/main/java/com/example/unit1/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.unit1
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.example.unit1.databinding.ActivityMainBinding
@@ -25,44 +28,67 @@ class MainActivity : AppCompatActivity() {
         val me = Person(name,age, country)
 
         binding.btnForward.setOnClickListener {
-
-            requestPermissions()
-
             Intent(this,SecondActivity::class.java).also {
                 it.putExtra("Extra_Me",me)
                 it.putExtra("Extra_Name",name)
                 startActivity(it)
             }
+        }
+        binding.btnRequests.setOnClickListener {
+            requestPermissions()
+        }
+        binding.btnGetImage.setOnClickListener {
+            Intent(Intent.ACTION_GET_CONTENT).also {
+                getContent.launch("image/*")
 
+            }
         }
 
     }
 
-    private fun hasExternalStoragePermission() =
+    //Permissions
+    private fun hasWriteExternalStoragePermission() =
         ActivityCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+    private fun hasReadExternalStoragePermission() =
+        ActivityCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
 
     private fun hasForegroundLocationPermission() =
         ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
 
     private fun hasBackgroundLocationPermission() =
-        ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
 
     private fun requestPermissions (){
-        var permissionsToRequest = mutableListOf<String>()
-        if(!hasExternalStoragePermission()){
+        val permissionsToRequest = mutableListOf<String>()
+
+        if(!hasWriteExternalStoragePermission()){
             permissionsToRequest.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        }
+        if(!hasReadExternalStoragePermission()){
+            permissionsToRequest.add(Manifest.permission.READ_EXTERNAL_STORAGE)
         }
         if(!hasForegroundLocationPermission()){
             permissionsToRequest.add(Manifest.permission.ACCESS_COARSE_LOCATION)
         }
         if(!hasBackgroundLocationPermission()){
-            permissionsToRequest.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                permissionsToRequest.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            }
         }
 
         if(permissionsToRequest.isNotEmpty()){
             ActivityCompat.requestPermissions(this,permissionsToRequest.toTypedArray(),0)
         }
     }
-
+    //Image Picker
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        println(uri)
+        binding.imageView.setImageURI(uri)
+    }
 
 }
